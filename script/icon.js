c9f3590125cad99c0c3d45f57f186fe3f53743c9
cheawon25 <99226711+cheawon25@users.noreply.github.com>
2022-04-14 11:27:01 +0900

    Add files via upload

---
+++ script/icon.js
@@ -0,0 +1,20 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 95) {
+        $('#main2').addClass('main2');
+    } else {
+        $('#main2').removeClass('main2');
+    }
+});
+
+/* $( '#main2' ).click( function() {
+	$( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+	return false;
+} ); */
+
+//메뉴 클릭 시 체크박스 해제
+$(function(){
+    $(".sidebar a").click(function(){
+        $('#menuicon').prop('checked',false);
+    });
+  });
---
+++ script/script.js
@@ -0,0 +1,10 @@
+/* window.onscroll = scrollF();
+
+$(function scrollF(){
+  if(document.body.scrollTop>95 || document.documentElement.scrollTop > 95){
+    document.getElementById('main2').style.top = '0';
+  }else{
+    document.getElementById('main2').style.top = '-100px'
+  }
+}); */
+
